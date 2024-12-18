b32489851220ebb44a733809686c22d21032027d
mac <mac@macs-MacBook-Pro-3.local>
2024-12-18 09:27:34 -0500

    updated install script

--- install.bat
+++ install.bat
@@ -2,3 +2,13 @@
 python -m venv env
 call env\Scripts\activate
 pip install -r requirements.txt
+
+set /p appName=APP_NAME: 
+set /p channelId=CHANNEL_ID: 
+set outputFile=.env
+(
+  echo APP_NAME=%appName%
+  echo CHANNEL_ID=%channelId%
+) > %outputFile%
+
+echo .env initialized
--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
+#!/bin/zsh
+
 cd "$(dirname "$0")";
 
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
+
+read "appName?APP_NAME: "
+read "channelId?CHANNEL_ID: "
+outputFile=".env"
+(
+  echo "APP_NAME=$appName"
+  echo "CHANNEL_ID=$channelId"
+) > $outputFile
+
+echo ".env initialized"
