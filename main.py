30f1f5ad940a8aaf44dfe348f52dee9fc87926d3
mac <mac@macs-MacBook-Pro-3.local>
2024-12-19 14:40:34 -0500

    added event id

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ load_dotenv()
 interface = None  # Change to match your actual network interface
 APP_NAME = os.environ['APP_NAME']
 CHANNEL_ID = bytes([int(os.environ['CHANNEL_ID'])])
+event_id = 0
 
 def list_interfaces():
     # This will print all available network interfaces
@@ -18,14 +19,17 @@ def list_interfaces():
     print(conf.ifaces)
 
 def send_arp_with_extra_data(custom_data):
+    global event_id
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Broadcast MAC address
     arp = ARP(op=1, hwsrc=ether.src, psrc="0.0.0.0", hwdst="00:00:00:00:00:00", pdst="0.0.0.0")
-    extra_data = CHANNEL_ID + custom_data
+    event_id_buf = bytes([event_id])
+    extra_data = CHANNEL_ID + event_id_buf + custom_data
     packet = ether / arp / extra_data
+    event_id = (event_id + 1) % 256
 
     # Send packet out on specified interface
     sendp(packet, iface=interface)
-    print(f"Sent ARP packet with extra data: {custom_data}")
+    print(f"Sent ARP packet with extra data: {event_id}")
 
 def on_message(ws, message):
     send_arp_with_extra_data(message)
