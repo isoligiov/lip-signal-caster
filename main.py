b4444a3ec25c1ecfb3d56a4ea97f3e205346da93
mac <mac@macs-MacBook-Pro-3.local>
2024-12-20 15:17:17 -0500

    reconnect logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import websocket
-from scapy.all import sendp, Ether, ARP, conf
+from scapy.all import sendp, Ether, ARP
 import os
 import rel
 import ssl
@@ -13,11 +13,6 @@ interface = None  # Change to match your actual network interface
 APP_NAME = os.environ['APP_NAME']
 CHANNEL_ID = bytes([int(os.environ['CHANNEL_ID'])])
 event_id = 0
-
-def list_interfaces():
-    # This will print all available network interfaces
-    print("Available network interfaces:")
-    print(conf.ifaces)
 
 def send_arp_with_extra_data(custom_data):
     global event_id
@@ -37,19 +32,18 @@ def on_message(ws, message):
 
 def on_error(ws, error):
     print(error)
-    time.sleep(5)
-    reconnect()
+
 
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
-    time.sleep(5)
-    reconnect()
 
 def on_open(ws):
     print("Opened connection")
     ws.send_text(f'dest/{APP_NAME}')
 
-def reconnect():
+if __name__ == "__main__":
+    websocket.enableTrace(False)
+    print(APP_NAME, CHANNEL_ID)
     ws = websocket.WebSocketApp(f"wss://streamlineanalytics.net:10010",
                               on_open=on_open,
                               on_message=on_message,
@@ -58,9 +52,5 @@ def reconnect():
 
     ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE}, dispatcher=rel, reconnect=5, ping_interval=10)
 
-if __name__ == "__main__":
-    websocket.enableTrace(False)
-    print(APP_NAME, CHANNEL_ID)
-    reconnect()
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
